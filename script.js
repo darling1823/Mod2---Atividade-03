e4870c05423b82685ed071871cc0d6a75b3d7e62
darling1823 <gabrielalcantara1823@gmail.com>
2022-06-16 09:41:11 -0300

    while added

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ for(let i = 1; i <= 10; i++){
     console.log(i)
 };
 
+let i = 10
+while(i > 1){
+    i -= 1
+    console.log(i)
+};
+
 for(let i = 1; i <= 100; i++){
     if(i % 2 !== 0)
     console.log(i)
 };
 
-for(let i = 1; i <= 100; i++){
+for(let i = 0; i <= 100; i++){
     if(i % 2 == 0)
     console.log(i)
 };
